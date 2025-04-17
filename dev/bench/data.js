15899100c66f2be60102769f2632b01eaa0641b0
github-action-benchmark <github@users.noreply.github.com>
2025-04-17 04:55:41 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt) (customSmallerIsBetter) benchmark result for 7dfed942ddc242fb0fb9adf0694ae25e9bdfc9da

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744797227959,
+  "lastUpdate": 1744865741418,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -551,6 +551,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 888563,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7dfed942ddc242fb0fb9adf0694ae25e9bdfc9da",
+          "message": "Merge pull request #165 from manastasova/mldsa_load_store_64\n\nAdd spec and proofs for load64 and store64",
+          "timestamp": "2025-04-17T05:53:00+01:00",
+          "tree_id": "5c21cb0d6a1262ddac7c07e7507913460e514aa5",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/7dfed942ddc242fb0fb9adf0694ae25e9bdfc9da"
+        },
+        "date": 1744865739500,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 301543,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 1041540,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 325653,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 558367,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1518195,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 532909,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 863783,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1966827,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 889863,
             "unit": "cycles"
           }
         ]
