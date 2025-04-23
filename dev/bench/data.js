e4b67b6d0036a5c9dfbf672eec16e7ddcee3a131
github-action-benchmark <github@users.noreply.github.com>
2025-04-23 17:29:06 +0800

    add Mac Mini (M1, 2020) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 811d91c9fa8dc566c04a5f45d8ba295bc06d9ceb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745397810820,
+  "lastUpdate": 1745400546169,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -1870,6 +1870,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 387388,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "811d91c9fa8dc566c04a5f45d8ba295bc06d9ceb",
+          "message": "Merge pull request #139 from pq-code-package/cbmc-unpack-sig2\n\nSimplification and proof of unpack_sig() and supporting functions.",
+          "timestamp": "2025-04-23T16:34:36+08:00",
+          "tree_id": "dced8030d3b2605abd7e1479abb230a9e60c55f0",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/811d91c9fa8dc566c04a5f45d8ba295bc06d9ceb"
+        },
+        "date": 1745400544906,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 129835,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 392547,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 140076,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 225780,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 647969,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 227618,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 376419,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 844941,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 392645,
             "unit": "cycles"
           }
         ]
