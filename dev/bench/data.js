a7d044c81b9c9708725b137748eeb12b46af86e0
github-action-benchmark <github@users.noreply.github.com>
2025-04-28 12:02:53 +0800

    add Mac Mini (M1, 2020) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 3d621b5a4335d39410639417eb5a46402178b2da

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745812663322,
+  "lastUpdate": 1745812973824,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -2629,6 +2629,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 388702,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3d621b5a4335d39410639417eb5a46402178b2da",
+          "message": "Merge pull request #178 from pq-code-package/ci-ec2\n\nCI: Add EC2 runners and move CBMC there",
+          "timestamp": "2025-04-28T04:51:46+01:00",
+          "tree_id": "d2d7f911f9d9e6f68e7e8e14d785b80bf2a0d3bb",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/3d621b5a4335d39410639417eb5a46402178b2da"
+        },
+        "date": 1745812972072,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 130709,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 395340,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 140937,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 228388,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 653457,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 228906,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 379003,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 849987,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 388638,
             "unit": "cycles"
           }
         ]
