9750434dd65c4799c2100ae9951cd6082f08c13b
github-action-benchmark <github@users.noreply.github.com>
2025-04-29 03:54:53 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 4c48eb7c1144a12fae98b5c2bd260cb5072d3886

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745898729187,
+  "lastUpdate": 1745898893496,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -1726,6 +1726,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 898624,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4c48eb7c1144a12fae98b5c2bd260cb5072d3886",
+          "message": "Merge pull request #183 from pq-code-package/mld_invntt_layer\n\nCMBC: Refactor, add contract and proof for invntt_tomont",
+          "timestamp": "2025-04-29T11:46:07+08:00",
+          "tree_id": "280fa4825ffc715fb5d9d2a1bf93ea0f28df59b3",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/4c48eb7c1144a12fae98b5c2bd260cb5072d3886"
+        },
+        "date": 1745898891469,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 308081,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 963997,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 332644,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 568382,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1563622,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 542568,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 873542,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 2028779,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 901972,
             "unit": "cycles"
           }
         ]
