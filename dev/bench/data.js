b3d0c91e66dd41ce91b45a697323e6be1c91937a
github-action-benchmark <github@users.noreply.github.com>
2025-05-02 15:43:31 +0800

    add Arm Cortex-A55 (Snapdragon 888) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 7f1d91aa9f823b70e6bab6947c0c0d8a1d468bf9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746171031874,
+  "lastUpdate": 1746171811959,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -3453,6 +3453,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 1433019,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7f1d91aa9f823b70e6bab6947c0c0d8a1d468bf9",
+          "message": "Merge pull request #191 from pq-code-package/shake128-init-absorb-final\n\nCBMC: Add proof and contract for shake128_init _absorb _finalize",
+          "timestamp": "2025-05-02T15:41:33+08:00",
+          "tree_id": "7e5b8c5f6e520d24eb1a437abe318d7509ccf568",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/7f1d91aa9f823b70e6bab6947c0c0d8a1d468bf9"
+        },
+        "date": 1746171809981,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 490071,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 1858795,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 563230,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 827394,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 2979311,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 881600,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 1341415,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 3794969,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 1432983,
             "unit": "cycles"
           }
         ]
