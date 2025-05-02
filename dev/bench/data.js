d0c662554323c4d74593f56360b168745e8ff798
github-action-benchmark <github@users.noreply.github.com>
2025-05-02 06:02:37 +0000

    add Arm Cortex-A55 (Snapdragon 888) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for f33d353093f110b48948caf59936fb4c64043f1a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746157421583,
+  "lastUpdate": 1746165757623,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -3039,6 +3039,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 1434069,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f33d353093f110b48948caf59936fb4c64043f1a",
+          "message": "Merge pull request #187 from pq-code-package/keccak-proofs-continue\n\nAdd spec and proofs for Keccak dependent functions",
+          "timestamp": "2025-05-02T13:59:23+08:00",
+          "tree_id": "282eddf79eddeeeabb60dfa5662437a58f3aa3ae",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/f33d353093f110b48948caf59936fb4c64043f1a"
+        },
+        "date": 1746165754824,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 492412,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 1854813,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 563528,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 826603,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 2971399,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 881039,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 1343132,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 3777983,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 1433877,
             "unit": "cycles"
           }
         ]
