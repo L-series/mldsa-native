b460ee8f686300df4d87c860c844caa7419758a0
github-action-benchmark <github@users.noreply.github.com>
2025-05-08 23:35:55 +0000

    add Intel Xeon 3rd gen (c6i) (customSmallerIsBetter) benchmark result for 82be3a17cdc19e9e01df6b8ead0d0217204fd165

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1746747352625,
+  "lastUpdate": 1746747355472,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -11396,6 +11396,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 456501,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodchap@amazon.com",
+            "name": "Roderick Chapman",
+            "username": "rod-chapman"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82be3a17cdc19e9e01df6b8ead0d0217204fd165",
+          "message": "Merge pull request #213 from pq-code-package/keccak_last\n\nOptimize proof for `keccak_squeezeblocks`",
+          "timestamp": "2025-05-09T00:33:25+01:00",
+          "tree_id": "8338221f8239d3c2ca673cdb03fb4f21afc3e426",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/82be3a17cdc19e9e01df6b8ead0d0217204fd165"
+        },
+        "date": 1746747354595,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 174685,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 486985,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 183476,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 300339,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 773373,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 297186,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 500776,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1022543,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 506440,
             "unit": "cycles"
           }
         ]
