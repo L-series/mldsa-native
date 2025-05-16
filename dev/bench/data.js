428da0b38d51b0ad47b6e842995c3921a0fcedcd
github-action-benchmark <github@users.noreply.github.com>
2025-05-16 08:19:04 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 62ba28cae9826420c9c7b9253608ed16facd62a6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747383533684,
+  "lastUpdate": 1747383544485,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -34328,6 +34328,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 702613,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "62ba28cae9826420c9c7b9253608ed16facd62a6",
+          "message": "Merge pull request #238 from pq-code-package/license_badge\n\nREADME: Update license badges",
+          "timestamp": "2025-05-16T16:14:40+08:00",
+          "tree_id": "632e6b6e8c6fe017f13238da8efaa6399472474e",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/62ba28cae9826420c9c7b9253608ed16facd62a6"
+        },
+        "date": 1747383542902,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 237938,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 541298,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 238726,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 431820,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 908318,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 404538,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 716902,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1239753,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 703555,
             "unit": "cycles"
           }
         ]
