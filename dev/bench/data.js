09016cc0af49741957018f0b706030b0c50b9b9e
github-action-benchmark <github@users.noreply.github.com>
2025-05-18 05:31:05 +0000

    add AMD EPYC 4th gen (c7a) (no-opt) (customSmallerIsBetter) benchmark result for de0522a9db423a5ee350a652ebc98232a7d52876

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747546251912,
+  "lastUpdate": 1747546265315,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -32593,6 +32593,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 339848,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "de0522a9db423a5ee350a652ebc98232a7d52876",
+          "message": "Merge pull request #251 from pq-code-package/poly_uniform_gamma1_4x\n\nImplement `polyvecl_uniform_gamma1` using 4-way Keccak",
+          "timestamp": "2025-05-18T06:27:51+01:00",
+          "tree_id": "347d702ff946e97713bd9b20d6b9e2053382310f",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/de0522a9db423a5ee350a652ebc98232a7d52876"
+        },
+        "date": 1747546264992,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 119912,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 367164,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 128289,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 201804,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 568620,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 201884,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 334772,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 747679,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 338004,
             "unit": "cycles"
           }
         ]
