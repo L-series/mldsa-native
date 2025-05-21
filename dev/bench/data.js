f8d8f8ae93b638a9d665b3531eb0c91cfebf7b20
github-action-benchmark <github@users.noreply.github.com>
2025-05-21 05:17:09 +0000

    add Intel Xeon 4th gen (c7i) (no-opt) (customSmallerIsBetter) benchmark result for 0bb572acd4dba277e55fe772bd703ebab90a1b18

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747804625948,
+  "lastUpdate": 1747804629726,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -32382,6 +32382,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 273668,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0bb572acd4dba277e55fe772bd703ebab90a1b18",
+          "message": "Merge pull request #254 from pq-code-package/cbmc-poly_uniform_4x\n\nCBMC: Add proof and spec for `poly_uniform_4x` and `poly_uniform_eta_4x`",
+          "timestamp": "2025-05-21T13:14:23+08:00",
+          "tree_id": "a1fb52358a16733e9c2b938928e61c18f9a09560",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/0bb572acd4dba277e55fe772bd703ebab90a1b18"
+        },
+        "date": 1747804629379,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 98448,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 282036,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 101896,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 165539,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 448591,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 164721,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 270885,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 585838,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 273404,
             "unit": "cycles"
           }
         ]
