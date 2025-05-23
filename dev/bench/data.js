a3e87b167fb3fec8e775fe803b1d7a9e80304f6b
github-action-benchmark <github@users.noreply.github.com>
2025-05-23 10:08:29 +0800

    add Mac Mini (M1, 2020) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for a1ad5922dc88d9f63e1cc7d5a6213ab6d0798c1a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747966091008,
+  "lastUpdate": 1747966109716,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -18223,6 +18223,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 334383,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a1ad5922dc88d9f63e1cc7d5a6213ab6d0798c1a",
+          "message": "Merge pull request #280 from pq-code-package/cbmc-poly_add-polyveck_add-polyvecl_add\n\nCBMC and refactor: Change poly_add and vector-variants and add proof",
+          "timestamp": "2025-05-23T10:07:39+08:00",
+          "tree_id": "b39a740969b6260936870a85845739a5deb2b23d",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/a1ad5922dc88d9f63e1cc7d5a6213ab6d0798c1a"
+        },
+        "date": 1747966108271,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 116518,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 369399,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 127125,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 200041,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 585208,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 202110,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 325439,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 751554,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 334484,
             "unit": "cycles"
           }
         ]
