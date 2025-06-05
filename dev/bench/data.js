19cef08dc08ff08c12c11f9834cfd779fd864d77
github-action-benchmark <github@users.noreply.github.com>
2025-06-05 18:18:28 +0800

    add Arm Cortex-A76 (Raspberry Pi 5) benchmarks (opt) (customSmallerIsBetter) benchmark result for bdb7ddd79d8375d4eeb0fa86bed2d4410d923414

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749118646956,
+  "lastUpdate": 1749118708590,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -75665,6 +75665,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 562493,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bdb7ddd79d8375d4eeb0fa86bed2d4410d923414",
+          "message": "Merge pull request #293 from pq-code-package/refactor_sign_hashing\n\nRefactor and simplify repeated hashing code in sign.c",
+          "timestamp": "2025-06-05T18:06:53+08:00",
+          "tree_id": "0a13f1615a24ab864f3c6ae643ec4c4fdcab7434",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/bdb7ddd79d8375d4eeb0fa86bed2d4410d923414"
+        },
+        "date": 1749118706442,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 196326,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 471772,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 204170,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 349729,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 768642,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 335845,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 575991,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1043511,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 574003,
             "unit": "cycles"
           }
         ]
