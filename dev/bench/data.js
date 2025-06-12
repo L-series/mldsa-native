053995adfdbd3227531aad7e1b2b80ee4d3c72e4
github-action-benchmark <github@users.noreply.github.com>
2025-06-12 04:30:43 +0000

    add Graviton4 (no-opt) (customSmallerIsBetter) benchmark result for e5a68c6ec42c871498a2a00357378b6883caecbb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749702636218,
+  "lastUpdate": 1749702643541,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -61094,6 +61094,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 382508,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e5a68c6ec42c871498a2a00357378b6883caecbb",
+          "message": "Merge pull request #302 from pq-code-package/keccak\n\nAdd native Keccak backend for x86_64 and AArch64",
+          "timestamp": "2025-06-12T05:27:25+01:00",
+          "tree_id": "e631eb4297a4daa909f129438fd6ccc122469310",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/e5a68c6ec42c871498a2a00357378b6883caecbb"
+        },
+        "date": 1749702643168,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 139991,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 388897,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 143141,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 227590,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 627017,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 231366,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 378011,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 814645,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 382527,
             "unit": "cycles"
           }
         ]
