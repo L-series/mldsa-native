e1c63afc5f22f4f26524c23c286270f4fa67d7e4
github-action-benchmark <github@users.noreply.github.com>
2025-06-16 10:09:01 +0000

    add Graviton3 (customSmallerIsBetter) benchmark result for 2bd00dc5475901ccb8f5b83ad7b3c8065a06eba0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750068539998,
+  "lastUpdate": 1750068541718,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -66685,6 +66685,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 249838,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2bd00dc5475901ccb8f5b83ad7b3c8065a06eba0",
+          "message": "Merge pull request #324 from pq-code-package/slothy_upgrade_13june2025\n\nUpgrade SLOTHY to the HEAD of its main branch, as of 13th June 2025.",
+          "timestamp": "2025-06-16T18:05:22+08:00",
+          "tree_id": "1be4d039b4422c6a143d882a0f3001befbcad198",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/2bd00dc5475901ccb8f5b83ad7b3c8065a06eba0"
+        },
+        "date": 1750068534633,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 86182,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 226057,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 96172,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 149560,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 369455,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 154321,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 243767,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 475678,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 249834,
             "unit": "cycles"
           }
         ]
