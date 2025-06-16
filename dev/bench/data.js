49f92e2b075bce6aba504e11b0ce96cd38b4dbd9
github-action-benchmark <github@users.noreply.github.com>
2025-06-16 09:31:03 +0000

    add Graviton4 (customSmallerIsBetter) benchmark result for d00867d3f21683c5d79dcde2909449dfc4a614a3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750066261265,
+  "lastUpdate": 1750066263590,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -58332,6 +58332,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 232732,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d00867d3f21683c5d79dcde2909449dfc4a614a3",
+          "message": "Merge pull request #323 from pq-code-package/remove-nistkat\n\nRemove the NISTKAT test",
+          "timestamp": "2025-06-16T17:27:57+08:00",
+          "tree_id": "9d2f609904378cb6372e8c3fe476d9f17828e8bb",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/d00867d3f21683c5d79dcde2909449dfc4a614a3"
+        },
+        "date": 1750066258573,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 81301,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 213542,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 88814,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 141561,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 345521,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 143392,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 230715,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 446839,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 232775,
             "unit": "cycles"
           }
         ]
