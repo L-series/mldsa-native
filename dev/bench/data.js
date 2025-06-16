4d5087211eaeb545774818f574f4e89bdfd2aa6e
github-action-benchmark <github@users.noreply.github.com>
2025-06-16 09:30:37 +0000

    add Intel Xeon 3rd gen (c6i) (customSmallerIsBetter) benchmark result for d00867d3f21683c5d79dcde2909449dfc4a614a3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750066128127,
+  "lastUpdate": 1750066237355,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -30854,6 +30854,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 211200,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d00867d3f21683c5d79dcde2909449dfc4a614a3",
+          "message": "Merge pull request #323 from pq-code-package/remove-nistkat\n\nRemove the NISTKAT test",
+          "timestamp": "2025-06-16T17:27:57+08:00",
+          "tree_id": "9d2f609904378cb6372e8c3fe476d9f17828e8bb",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/d00867d3f21683c5d79dcde2909449dfc4a614a3"
+        },
+        "date": 1750066231804,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 77820,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 188954,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 86506,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 133698,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 314418,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 135768,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 209491,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 390941,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 211324,
             "unit": "cycles"
           }
         ]
