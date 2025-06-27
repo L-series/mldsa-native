a82fd041631c1a1099d74dd97677ed7f79e88dc6
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 05:55:26 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for 1eb50eb08852c7d2bce5b93028d6e17a7dd333ab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751003674567,
+  "lastUpdate": 1751003726327,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -39345,6 +39345,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 129950,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1eb50eb08852c7d2bce5b93028d6e17a7dd333ab",
+          "message": "Merge pull request #341 from pq-code-package/test-cbmc-6.7.0\n\nCBMC: Update to CBMC 6.7.0",
+          "timestamp": "2025-06-27T06:53:04+01:00",
+          "tree_id": "bb02d5787ad45762a1567e52c57b20eba0530afb",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/1eb50eb08852c7d2bce5b93028d6e17a7dd333ab"
+        },
+        "date": 1751003725008,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 47831,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 154119,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 54014,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 84984,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 253722,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 84621,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 127549,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 307234,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 129884,
             "unit": "cycles"
           }
         ]
