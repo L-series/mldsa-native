fa6f918b671b14a331d004d90a4833ace37c08b8
github-action-benchmark <github@users.noreply.github.com>
2025-06-27 14:03:52 +0000

    add AMD EPYC 3rd gen (c6a) (no-opt) (customSmallerIsBetter) benchmark result for 429d9d3dae9746cf9d28ad7aa8ca2b7a74ac2f2e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751033028881,
+  "lastUpdate": 1751033032511,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -56810,6 +56810,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 384979,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "429d9d3dae9746cf9d28ad7aa8ca2b7a74ac2f2e",
+          "message": "Merge pull request #345 from pq-code-package/refactor_matrix_expand\n\nRefactor poly_uniform_4x() to explicitly return 4 polynomials",
+          "timestamp": "2025-06-27T22:00:38+08:00",
+          "tree_id": "372b2a65ecf9f5a8e90bfe393324f403cde89346",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/429d9d3dae9746cf9d28ad7aa8ca2b7a74ac2f2e"
+        },
+        "date": 1751033032123,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 133967,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 508322,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 149359,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 223353,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 811400,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 233022,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 368604,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1026244,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 381143,
             "unit": "cycles"
           }
         ]
