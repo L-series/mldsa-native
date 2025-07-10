c942fd299a8c0e77ae40828a0b4adc85f5281a7f
github-action-benchmark <github@users.noreply.github.com>
2025-07-10 08:58:08 +0000

    add Graviton2 (no-opt) (customSmallerIsBetter) benchmark result for 4b18d5cb4ab4ca4cf8a824bd478cbe918ab5f130

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752137887180,
+  "lastUpdate": 1752137888779,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -93946,6 +93946,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 614262,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4b18d5cb4ab4ca4cf8a824bd478cbe918ab5f130",
+          "message": "Merge pull request #353 from pq-code-package/cbmc-crypto_sign_verify\n\nCBMC: Add the spec and proof for crypto_sign_verify, crypto_sign_verify_extmu",
+          "timestamp": "2025-07-10T16:54:02+08:00",
+          "tree_id": "fafef43544eb84155369a7af818228e89bccdd4c",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/4b18d5cb4ab4ca4cf8a824bd478cbe918ab5f130"
+        },
+        "date": 1752137887440,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 210514,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 722222,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 238963,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 376776,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1187460,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 370447,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 596588,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1517452,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 614314,
             "unit": "cycles"
           }
         ]
