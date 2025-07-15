2d110a78797a751b33938cb08d73cabfab74da4d
github-action-benchmark <github@users.noreply.github.com>
2025-07-15 01:13:35 +0000

    add Intel Xeon 4th gen (c7i) (no-opt) (customSmallerIsBetter) benchmark result for f2d8abde4cb2638c148952106dce4341c8626f72

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752541997937,
+  "lastUpdate": 1752542015019,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -62604,6 +62604,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 270892,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f2d8abde4cb2638c148952106dce4341c8626f72",
+          "message": "Merge pull request #348 from pq-code-package/aarch64-rej-eta-implementation\n\nAdd native imlementation of rej_eta (AArch64 + AVX2)",
+          "timestamp": "2025-07-15T09:10:54+08:00",
+          "tree_id": "bb3b4648f46d59e12a7683f79dc76dcdb32ce731",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/f2d8abde4cb2638c148952106dce4341c8626f72"
+        },
+        "date": 1752542014757,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 96133,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 323398,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 102521,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 164220,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 528496,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 163672,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 267417,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 668661,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 270775,
             "unit": "cycles"
           }
         ]
