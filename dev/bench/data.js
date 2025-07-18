9272d9faedf63911ba62beaf1253615dd4cf2afc
github-action-benchmark <github@users.noreply.github.com>
2025-07-18 09:42:14 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for 387fbd5a7ed2c6a2ebf9569f93dff21b78b954c7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752831683573,
+  "lastUpdate": 1752831734529,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -51144,6 +51144,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 104294,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "387fbd5a7ed2c6a2ebf9569f93dff21b78b954c7",
+          "message": "Merge pull request #374 from pq-code-package/cbmc-prove_sign_toplevel\n\nAdd contracts and proofs for top-level signature functions",
+          "timestamp": "2025-07-18T17:39:47+08:00",
+          "tree_id": "1ed33dbb81a8a8b0904b7e297156d3c5c7bf3230",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/387fbd5a7ed2c6a2ebf9569f93dff21b78b954c7"
+        },
+        "date": 1752831731817,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 35716,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 136720,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 45705,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 62226,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 227635,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 69839,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 94279,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 266948,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 104316,
             "unit": "cycles"
           }
         ]
