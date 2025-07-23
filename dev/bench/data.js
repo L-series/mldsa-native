d89a57de1eb55f397ba904601b1a64ed95d825c5
github-action-benchmark <github@users.noreply.github.com>
2025-07-23 04:48:29 +0000

    add AMD EPYC 4th gen (c7a) (customSmallerIsBetter) benchmark result for 8dea0d21d9448b03b148d94480703a01a56ee43d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753246102283,
+  "lastUpdate": 1753246109166,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -62878,6 +62878,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 120173,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8dea0d21d9448b03b148d94480703a01a56ee43d",
+          "message": "Merge pull request #378 from pq-code-package/cbmc-issue-crypto_sign_open_s_eq_sm\n\nCBMC: Allow `m == sm` aliasing for crypto_sign_open",
+          "timestamp": "2025-07-23T12:44:54+08:00",
+          "tree_id": "148b055705d627c9f87862fa1403c8259d2a11a4",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/8dea0d21d9448b03b148d94480703a01a56ee43d"
+        },
+        "date": 1753246101139,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 41220,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 151992,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 53819,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 72691,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 241350,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 82110,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 109802,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 283659,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 123540,
             "unit": "cycles"
           }
         ]
