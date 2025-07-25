0527a2999ba13e2450f66012e33f1c8432b8b79e
github-action-benchmark <github@users.noreply.github.com>
2025-07-25 14:08:26 +0800

    add SpacemiT K1 8 (Banana Pi F3) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 5070d3b20e0ffd3d8dd6b4a0d64fe62adf328fb0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753423509810,
+  "lastUpdate": 1753423705685,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -26087,6 +26087,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 2725610,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5070d3b20e0ffd3d8dd6b4a0d64fe62adf328fb0",
+          "message": "Merge pull request #373 from pq-code-package/doc_add-docstring_polyvec_h\n\nAdd missing docstrings in polyvec.h",
+          "timestamp": "2025-07-25T13:59:22+08:00",
+          "tree_id": "8d6fdef324a7b482df07cff65903d59dbaa624b6",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/5070d3b20e0ffd3d8dd6b4a0d64fe62adf328fb0"
+        },
+        "date": 1753423685827,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 944767,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 4356927,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 1075148,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 1570198,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 7180906,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 1700762,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 2546059,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 8845960,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 2725533,
             "unit": "cycles"
           }
         ]
