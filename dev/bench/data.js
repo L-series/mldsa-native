db7dfab35ea7b823d8a93c467d07f299dd12eef0
github-action-benchmark <github@users.noreply.github.com>
2025-07-28 02:14:41 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 334c7a2afd13eddb3535814f708fdcde502a98df

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753668877295,
+  "lastUpdate": 1753668880934,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -108986,6 +108986,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 353751,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "334c7a2afd13eddb3535814f708fdcde502a98df",
+          "message": "Merge pull request #390 from pq-code-package/shasum\n\nMakefile: Detect suitable sha256sum tool and set -o pipefail",
+          "timestamp": "2025-07-28T10:11:12+08:00",
+          "tree_id": "33ebcdc0243eef6f2bc0061eeb7395a5d2f2617b",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/334c7a2afd13eddb3535814f708fdcde502a98df"
+        },
+        "date": 1753668872116,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 117704,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 419317,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 137009,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 202971,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 691577,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 220514,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 331100,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 869010,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 354605,
             "unit": "cycles"
           }
         ]
