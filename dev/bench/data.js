66fca4295747b99163d67f552672eb818882596c
github-action-benchmark <github@users.noreply.github.com>
2025-07-30 10:09:13 +0000

    add Graviton4 (no-opt) (customSmallerIsBetter) benchmark result for 77976820d20cf173c7fe466a6f272565764ec5cf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753870151772,
+  "lastUpdate": 1753870153539,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -95042,6 +95042,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 390899,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "77976820d20cf173c7fe466a6f272565764ec5cf",
+          "message": "Merge pull request #392 from pq-code-package/constant-time-2\n\nCT: Make poly_chknorm constant flow",
+          "timestamp": "2025-07-30T11:05:38+01:00",
+          "tree_id": "ad7861270de2ad3cf88e2d7f36365b0b818428e8",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/77976820d20cf173c7fe466a6f272565764ec5cf"
+        },
+        "date": 1753870152307,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 134695,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 508591,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 149572,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 228747,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 820294,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 237341,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 377474,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1030357,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 390812,
             "unit": "cycles"
           }
         ]
