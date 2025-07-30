782d2346691f0390e88324c27fda51db04e2a56e
github-action-benchmark <github@users.noreply.github.com>
2025-07-30 06:18:30 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt) (customSmallerIsBetter) benchmark result for 6369a7e6484decca63ab153ce12143a42fd5b56a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753856306891,
+  "lastUpdate": 1753856310350,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -5933,6 +5933,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 670999,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6369a7e6484decca63ab153ce12143a42fd5b56a",
+          "message": "Merge pull request #371 from pq-code-package/constant-time\n\n Constant-time hardening for `caddq`, `poly_chknorm`, `decompose`",
+          "timestamp": "2025-07-30T07:14:56+01:00",
+          "tree_id": "7fddc8def627ecca95aad02bca227a1fe2f0003b",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/6369a7e6484decca63ab153ce12143a42fd5b56a"
+        },
+        "date": 1753856248884,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 230663,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 798939,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 258330,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 412392,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1319551,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 430737,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 680810,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1698894,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 698062,
             "unit": "cycles"
           }
         ]
