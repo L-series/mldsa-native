9aab89c4b02d00f075bafaab9156f184cdb6dbcd
github-action-benchmark <github@users.noreply.github.com>
2025-08-04 13:04:56 +0800

    add Arm Cortex-A76 (Raspberry Pi 5) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 108d2d3e987be9699f8c0cd2301a424b3980cc88

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754283855650,
+  "lastUpdate": 1754283895856,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -41133,6 +41133,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 638095,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "108d2d3e987be9699f8c0cd2301a424b3980cc88",
+          "message": "Merge pull request #393 from pq-code-package/test-addCITest-PCT-zeroization\n\nCI: add the PCT enabled, PCT enabled + broken, Custom zeroization (explicit_bzero)",
+          "timestamp": "2025-08-04T12:58:56+08:00",
+          "tree_id": "6bd4ccc4ba4fde79664b47f93e5257e480c2b884",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/108d2d3e987be9699f8c0cd2301a424b3980cc88"
+        },
+        "date": 1754283894503,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 214931,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 797190,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 239916,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 384108,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1313611,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 385437,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 612519,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1665118,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 638211,
             "unit": "cycles"
           }
         ]
