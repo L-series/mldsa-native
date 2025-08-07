2f376f1d2a8f72d0a7a5c75d6eebe3277fe94843
github-action-benchmark <github@users.noreply.github.com>
2025-08-07 18:57:27 +0800

    add SpacemiT K1 8 (Banana Pi F3) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 17368828d884eec727657505c51afeb3845d89c2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754563534293,
+  "lastUpdate": 1754564246229,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -28226,6 +28226,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 2750040,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "17368828d884eec727657505c51afeb3845d89c2",
+          "message": "Merge pull request #415 from pq-code-package/acvp-on-the-fly\n\nACVP: Download testvectors transparently on the fly (Port mlkem-native PR#1119)",
+          "timestamp": "2025-08-07T18:38:47+08:00",
+          "tree_id": "3c18b501428d31cc86398f85e1fe46b26cb3fbb0",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/17368828d884eec727657505c51afeb3845d89c2"
+        },
+        "date": 1754564244904,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 830867,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 3361137,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 930654,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 1394406,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 5490767,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 1482041,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 2310541,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 6926078,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 2437273,
             "unit": "cycles"
           }
         ]
