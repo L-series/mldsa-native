fb72a66021f394177fee9b4cbb086735e80d3414
github-action-benchmark <github@users.noreply.github.com>
2025-09-10 07:58:09 +0000

    add Intel Xeon 4th gen (c7i) (no-opt) (customSmallerIsBetter) benchmark result for a7c9c157a84acaca78cd9c629bec5c4e257fb614

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757491085795,
+  "lastUpdate": 1757491089653,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -82890,6 +82890,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 280534,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a7c9c157a84acaca78cd9c629bec5c4e257fb614",
+          "message": "Merge pull request #443 from pq-code-package/bitwuzla-0.8.2\n\nbitwuzla: update the bitwuzla version from 0.7.0 to 0.8.2 in nix",
+          "timestamp": "2025-09-10T15:55:17+08:00",
+          "tree_id": "a5f0faa1c5797778e4cc21b9673afecb32e856cc",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/a7c9c157a84acaca78cd9c629bec5c4e257fb614"
+        },
+        "date": 1757491089342,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 96295,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 350837,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 105421,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 164320,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 577320,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 170524,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 274183,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 734164,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 280779,
             "unit": "cycles"
           }
         ]
