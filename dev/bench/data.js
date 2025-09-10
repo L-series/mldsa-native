c1c8936556c0fc7b52cae2b2545d7280c37c3809
github-action-benchmark <github@users.noreply.github.com>
2025-09-10 09:12:35 +0000

    add SpacemiT K1 8 (Banana Pi F3) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for a7c9c157a84acaca78cd9c629bec5c4e257fb614

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757491240465,
+  "lastUpdate": 1757495554892,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -29882,6 +29882,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 2433575,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a7c9c157a84acaca78cd9c629bec5c4e257fb614",
+          "message": "Merge pull request #443 from pq-code-package/bitwuzla-0.8.2\n\nbitwuzla: update the bitwuzla version from 0.7.0 to 0.8.2 in nix",
+          "timestamp": "2025-09-10T15:55:17+08:00",
+          "tree_id": "a5f0faa1c5797778e4cc21b9673afecb32e856cc",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/a7c9c157a84acaca78cd9c629bec5c4e257fb614"
+        },
+        "date": 1757495483936,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 831939,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 3357341,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 931922,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 1393718,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 5459875,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 1481177,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 2309962,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 6911094,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 2437148,
             "unit": "cycles"
           }
         ]
