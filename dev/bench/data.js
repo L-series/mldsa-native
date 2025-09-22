e329d82e762c846baccdcb125c2eb91dfe23bff5
github-action-benchmark <github@users.noreply.github.com>
2025-09-22 15:15:44 +0800

    add Arm Cortex-A55 (Snapdragon 888) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 1f82952455d55bb4e2540c2798a027f6f065075b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758525335552,
+  "lastUpdate": 1758525344375,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -23463,6 +23463,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 1375166,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1f82952455d55bb4e2540c2798a027f6f065075b",
+          "message": "Merge pull request #469 from pq-code-package/fix-cbmc-polyvec_matrix_pointwise_montgomery\n\nCBMC: Increase CBMC_OBJECT_BITS to improve proof runtime",
+          "timestamp": "2025-09-22T15:12:20+08:00",
+          "tree_id": "61a40821241f6c22d8e542ac32f4a4c7869b59d5",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/1f82952455d55bb4e2540c2798a027f6f065075b"
+        },
+        "date": 1758525342437,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 460861,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 2206784,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 544595,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 774080,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 3625117,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 846585,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 1250276,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 4498425,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 1364347,
             "unit": "cycles"
           }
         ]
