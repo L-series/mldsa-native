c0c120d15cb6e4200bdbeccbffe1af8f15973645
github-action-benchmark <github@users.noreply.github.com>
2025-09-22 10:51:29 +0800

    add Mac Mini (M1, 2020) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 3cf11f4e8563a3ffd5a0de51fd45bbbcd3f3b877

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758509471767,
+  "lastUpdate": 1758509488966,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -38095,6 +38095,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 328493,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3cf11f4e8563a3ffd5a0de51fd45bbbcd3f3b877",
+          "message": "Merge pull request #413 from pq-code-package/test-color-cbmc\n\nImprove UX of cbmc scripts, lint, autogen, format (align with mlkem-native)",
+          "timestamp": "2025-09-22T10:50:41+08:00",
+          "tree_id": "362171e2dc67273778243360ff017f0cf97c93e5",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/3cf11f4e8563a3ffd5a0de51fd45bbbcd3f3b877"
+        },
+        "date": 1758509487578,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 115037,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 430527,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 122122,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 197475,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 701052,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 197661,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 334661,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 883908,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 328501,
             "unit": "cycles"
           }
         ]
