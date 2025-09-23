4ebed9ef376c479df1b088eb466299f50e294a3b
github-action-benchmark <github@users.noreply.github.com>
2025-09-23 14:49:13 +0000

    add AMD EPYC 3rd gen (c6a) (customSmallerIsBetter) benchmark result for ebede4da5d24501212aed8ff760683b86078add1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758638946301,
+  "lastUpdate": 1758638953135,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -64183,6 +64183,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 220096,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ebede4da5d24501212aed8ff760683b86078add1",
+          "message": "Merge pull request #411 from pq-code-package/decompose-asm\n\nAdd native implementation of `poly_decompose`",
+          "timestamp": "2025-09-23T22:46:16+08:00",
+          "tree_id": "0f3a984015a0bb007503b95fb2831dbe2ef76c59",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/ebede4da5d24501212aed8ff760683b86078add1"
+        },
+        "date": 1758638938214,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 72429,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 235885,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 81935,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 127155,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 392445,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 135767,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 209507,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 490112,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 218227,
             "unit": "cycles"
           }
         ]
