3d6b00bb004530799dda5057303cf3fa0772a25a
github-action-benchmark <github@users.noreply.github.com>
2025-09-25 17:51:35 +0800

    add Arm Cortex-A55 (Snapdragon 888) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 899852cf938ec693ecad5aa892cf2c108779c3dc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758793817449,
+  "lastUpdate": 1758793894496,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -25740,6 +25740,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 1369153,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "899852cf938ec693ecad5aa892cf2c108779c3dc",
+          "message": "Merge pull request #478 from pq-code-package/fips202-external-api\n\nConfiguration: Add MLD_CONFIG_INTERNAL/EXTERNAL_API_QUALIFIER",
+          "timestamp": "2025-09-25T17:45:48+08:00",
+          "tree_id": "0ef4bfeea169d9e5d943df1aaeadec0b80a86a15",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/899852cf938ec693ecad5aa892cf2c108779c3dc"
+        },
+        "date": 1758793892815,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 465987,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 2221884,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 546513,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 777232,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 3632312,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 853385,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 1254472,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 4470799,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 1366465,
             "unit": "cycles"
           }
         ]
