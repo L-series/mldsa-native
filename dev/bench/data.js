26c5754ff8a09c7871541c4bae0cc6c1c9c83335
github-action-benchmark <github@users.noreply.github.com>
2025-09-27 05:21:56 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 17b2c76bcfc29eeb86c151da2d6ff4e038e6af3d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758950391831,
+  "lastUpdate": 1758950515518,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -17527,6 +17527,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 884651,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "17b2c76bcfc29eeb86c151da2d6ff4e038e6af3d",
+          "message": "Merge pull request #414 from pq-code-package/caddq-asm\n\nAdd native implementation for `poly_caddq`",
+          "timestamp": "2025-09-27T13:10:51+08:00",
+          "tree_id": "a136a55f8a3477df56c8f02164dcf11983d0840b",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/17b2c76bcfc29eeb86c151da2d6ff4e038e6af3d"
+        },
+        "date": 1758950512461,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 307015,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 1219904,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 342585,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 573506,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 2009002,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 540792,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 866282,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 2531018,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 902666,
             "unit": "cycles"
           }
         ]
