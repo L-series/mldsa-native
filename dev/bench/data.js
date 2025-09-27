fcd47696bdadb828dc68dd7e3c7016cf92be0449
github-action-benchmark <github@users.noreply.github.com>
2025-09-27 03:00:44 +0000

    add Graviton3 (customSmallerIsBetter) benchmark result for 6e4570f69500b1414ca91b979e9069746ac395dd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758942036090,
+  "lastUpdate": 1758942044311,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -136513,6 +136513,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 230657,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6e4570f69500b1414ca91b979e9069746ac395dd",
+          "message": "Merge pull request #481 from pq-code-package/cbmc-add-make-smt\n\nAdd Makefile targets to generate SMT files from CBMC",
+          "timestamp": "2025-09-27T10:57:14+08:00",
+          "tree_id": "80c4e1a16b47607d55973b0e1a021db93aaba466",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/6e4570f69500b1414ca91b979e9069746ac395dd"
+        },
+        "date": 1758942021154,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 76429,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 269772,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 89242,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 133695,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 448399,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 144105,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 216150,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 558655,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 230685,
             "unit": "cycles"
           }
         ]
