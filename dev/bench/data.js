d2a2b7b89af1025014567479912d91956641cc19
github-action-benchmark <github@users.noreply.github.com>
2025-09-30 14:09:11 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for 335fb12a0c1d1522fc07d3498fb0d63d22bc6e86

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759241230332,
+  "lastUpdate": 1759241351762,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -79986,6 +79986,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 103121,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "335fb12a0c1d1522fc07d3498fb0d63d22bc6e86",
+          "message": "Merge pull request #502 from pq-code-package/cbmc-remove-shake128\n\nCBMC: Remove leftover shake128 Makefile",
+          "timestamp": "2025-09-30T22:03:49+08:00",
+          "tree_id": "4a91885a1683183d128d6cc632662e3693808146",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/335fb12a0c1d1522fc07d3498fb0d63d22bc6e86"
+        },
+        "date": 1759241346844,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 35185,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 144037,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 42489,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 63760,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 241426,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 67975,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 96848,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 290019,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 102604,
             "unit": "cycles"
           }
         ]
