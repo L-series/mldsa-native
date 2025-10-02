8b0e18f134d522e68e951f3242c78cc96c5c2de5
github-action-benchmark <github@users.noreply.github.com>
2025-10-02 08:23:45 +0000

    add Graviton3 (no-opt) (customSmallerIsBetter) benchmark result for fe95e5a9f0db82603900cf4aeab399f7465dca7b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759393401783,
+  "lastUpdate": 1759393425247,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -156870,6 +156870,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 402254,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fe95e5a9f0db82603900cf4aeab399f7465dca7b",
+          "message": "Merge pull request #496 from pq-code-package/acvp-internal\n\nACVP: Add internal test cases",
+          "timestamp": "2025-10-02T16:19:08+08:00",
+          "tree_id": "f7bab5acebecb47536ac5cd9fc1f9f5bd9d8138e",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/fe95e5a9f0db82603900cf4aeab399f7465dca7b"
+        },
+        "date": 1759393424788,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 138574,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 495478,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 148743,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 241449,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 809883,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 240905,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 396445,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1031363,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 402249,
             "unit": "cycles"
           }
         ]
