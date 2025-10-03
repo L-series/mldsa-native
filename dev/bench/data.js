8475c772d4c5dac0e2bb3767f1898d30e9ec9886
github-action-benchmark <github@users.noreply.github.com>
2025-10-03 13:04:26 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt) (customSmallerIsBetter) benchmark result for 5fc3ec87de34e56985a2fd963e79b9bed68a15f4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759496654218,
+  "lastUpdate": 1759496665459,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -9245,6 +9245,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 691909,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5fc3ec87de34e56985a2fd963e79b9bed68a15f4",
+          "message": "Merge pull request #525 from pq-code-package/macos-15-intel\n\nSwitch mac (x86_64) github runner from `macos-13` to `macos-15-intel`",
+          "timestamp": "2025-10-03T20:59:41+08:00",
+          "tree_id": "c175960d85c2203188a3601d3dfeb245dcd7e13b",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/5fc3ec87de34e56985a2fd963e79b9bed68a15f4"
+        },
+        "date": 1759496538185,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 231654,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 734748,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 239142,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 394381,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1185187,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 394728,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 665601,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1578258,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 664367,
             "unit": "cycles"
           }
         ]
