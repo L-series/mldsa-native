92b9e379c1403049884ac6db7ade67ef56d8e581
github-action-benchmark <github@users.noreply.github.com>
2025-10-03 14:18:32 +0800

    add Mac Mini (M1, 2020) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 25fa4bc4d7bb96d5dfea040c0773176d0f57fa80

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759472294618,
+  "lastUpdate": 1759472312233,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -48514,6 +48514,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 328987,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "25fa4bc4d7bb96d5dfea040c0773176d0f57fa80",
+          "message": "Merge pull request #518 from pq-code-package/fix-slow-liboqs-ci\n\nOQS CI: Fix slowness of valgrind tests (constant-time and memory leak)",
+          "timestamp": "2025-10-03T14:17:46+08:00",
+          "tree_id": "bd0c39f4f04a09eb54f498b4b1630e3faece2b78",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/25fa4bc4d7bb96d5dfea040c0773176d0f57fa80"
+        },
+        "date": 1759472310833,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 115037,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 430815,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 122172,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 196906,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 701295,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 197629,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 335106,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 884829,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 328961,
             "unit": "cycles"
           }
         ]
