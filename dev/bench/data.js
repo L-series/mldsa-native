4c8c170017e9fd6e1cdecde32d593ce7d5372e91
github-action-benchmark <github@users.noreply.github.com>
2025-10-09 08:22:33 +0800

    add Mac Mini (M1, 2020) benchmarks (opt) (customSmallerIsBetter) benchmark result for d6f0aff7497adf6be7d29f4d061961e1b2f1048e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759918221329,
+  "lastUpdate": 1759969353109,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -217945,6 +217945,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 137859,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6f0aff7497adf6be7d29f4d061961e1b2f1048e",
+          "message": "Merge pull request #532 from pq-code-package/config-multilevel-build\n\nAdd MLD_CONFIG_MULTILEVEL_WITH_SHARED and NO_SHARED",
+          "timestamp": "2025-10-09T08:22:03+08:00",
+          "tree_id": "2083a9c9a25a5ba83a6067d4a6c14b5ce8302701",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/d6f0aff7497adf6be7d29f4d061961e1b2f1048e"
+        },
+        "date": 1759969351637,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 47864,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 157629,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 52369,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 83696,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 255541,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 85600,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 135671,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 321128,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 137903,
             "unit": "cycles"
           }
         ]
