16340ef6a7b40c93de3509dfadc53d98ae0c92ad
github-action-benchmark <github@users.noreply.github.com>
2025-10-11 19:04:03 +0000

    add Graviton4 (customSmallerIsBetter) benchmark result for abf8281191e9a0b04642705886d12f422d01f476

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760209441769,
+  "lastUpdate": 1760209443789,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -145824,6 +145824,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 201926,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "abf8281191e9a0b04642705886d12f422d01f476",
+          "message": "Merge pull request #510 from pq-code-package/polyz-unpack-asm\n\nAdd native implementation of polyz_unpack",
+          "timestamp": "2025-10-11T20:00:46+01:00",
+          "tree_id": "f34f08f66b16717de1e0c01744a13f2cb67662ae",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/abf8281191e9a0b04642705886d12f422d01f476"
+        },
+        "date": 1760209415762,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 69452,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 214770,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 72508,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 122780,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 353195,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 120430,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 200488,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 451124,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 198358,
             "unit": "cycles"
           }
         ]
