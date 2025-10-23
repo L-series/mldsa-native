4cbedf22e45b285bf72c4aef84f7c1792e9eaf13
github-action-benchmark <github@users.noreply.github.com>
2025-10-23 01:34:21 +0000

    add AMD EPYC 4th gen (c7a) (no-opt) (customSmallerIsBetter) benchmark result for a74d2dbb4f4c991aa116c4eb5549fbafcbd447bf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761183259406,
+  "lastUpdate": 1761183261543,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -137680,6 +137680,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 349377,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a74d2dbb4f4c991aa116c4eb5549fbafcbd447bf",
+          "message": "Merge pull request #468 from pq-code-package/avx2-pointwise-acc\n\nAVX2/Neon: Add native `pointwise` and `pointwise_acc native`",
+          "timestamp": "2025-10-23T09:30:33+08:00",
+          "tree_id": "0de149932636b03c5c5f07550453c8636a5e5d32",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/a74d2dbb4f4c991aa116c4eb5549fbafcbd447bf"
+        },
+        "date": 1761183259626,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 120468,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 452781,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 132362,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 204463,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 738445,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 209945,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 338579,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 942238,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 349756,
             "unit": "cycles"
           }
         ]
