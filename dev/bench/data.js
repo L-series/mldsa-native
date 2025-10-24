52b3a472687875abdd76a7e67b36662a8c2f5c58
github-action-benchmark <github@users.noreply.github.com>
2025-10-24 08:40:58 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for f0961e7d6e71da23cce0e8866745fc833c6cf378

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761295250124,
+  "lastUpdate": 1761295258737,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -91164,6 +91164,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 95098,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f0961e7d6e71da23cce0e8866745fc833c6cf378",
+          "message": "Merge pull request #553 from pq-code-package/mingw-14-15\n\nMingw-w64: Add 14.2.0 and 15.2.0",
+          "timestamp": "2025-10-24T09:38:29+01:00",
+          "tree_id": "0267c5988f263c89b705a70c7432cb2b192194d7",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/f0961e7d6e71da23cce0e8866745fc833c6cf378"
+        },
+        "date": 1761295252530,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 34830,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 120989,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 38282,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 62958,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 201926,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 62665,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 94342,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 232667,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 94979,
             "unit": "cycles"
           }
         ]
