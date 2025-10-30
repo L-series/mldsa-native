b6b6d976c32d5e45c28e0c20e1f879cb0bd0544e
github-action-benchmark <github@users.noreply.github.com>
2025-10-30 03:14:49 +0000

    add Intel Xeon 3rd gen (c6i) (no-opt) (customSmallerIsBetter) benchmark result for 257c38db25287f55b1e791907075d51df3db8b05

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761794083933,
+  "lastUpdate": 1761794089396,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -105587,6 +105587,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 462952,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "257c38db25287f55b1e791907075d51df3db8b05",
+          "message": "Merge pull request #565 from pq-code-package/nttunpack-macro\n\nx86: Rewrite nttunpack to use a macro instead of function calls",
+          "timestamp": "2025-10-30T11:10:24+08:00",
+          "tree_id": "dcc6c50dd1caccb58d58ba9359309278a2875e9f",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/257c38db25287f55b1e791907075d51df3db8b05"
+        },
+        "date": 1761794088795,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 157836,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 565397,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 169718,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 270682,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 926783,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 275658,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 454048,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1186994,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 463059,
             "unit": "cycles"
           }
         ]
