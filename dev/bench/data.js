ab3834f5274d86699db6c4bf55ebe34590987ebb
github-action-benchmark <github@users.noreply.github.com>
2025-10-30 11:09:46 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for 0bfa77a81c7795e49dbda60a5d1ffc97f24b85b5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761822577619,
+  "lastUpdate": 1761822586696,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -98547,6 +98547,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 94346,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0bfa77a81c7795e49dbda60a5d1ffc97f24b85b5",
+          "message": "Merge pull request #562 from pq-code-package/cfify\n\nAdd CFI information to assembly",
+          "timestamp": "2025-10-30T19:07:16+08:00",
+          "tree_id": "eeac1c838afb280388c48b99ff230610b2052105",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/0bfa77a81c7795e49dbda60a5d1ffc97f24b85b5"
+        },
+        "date": 1761822578942,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 35119,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 117877,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 37808,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 62724,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 201395,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 62626,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 94897,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 233897,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 94005,
             "unit": "cycles"
           }
         ]
