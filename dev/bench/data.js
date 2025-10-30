7b127633a735232ac4e4d4eb091885b8029fb841
github-action-benchmark <github@users.noreply.github.com>
2025-10-30 08:36:09 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt) (customSmallerIsBetter) benchmark result for f6d6c37e812898127dccb33ffc2cabd47bb1776e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761813310197,
+  "lastUpdate": 1761813368198,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -10418,6 +10418,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 651332,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f6d6c37e812898127dccb33ffc2cabd47bb1776e",
+          "message": "Merge pull request #569 from pq-code-package/hol_light_interactive\n\nHOL-Light: Allow interactive use of hol.sh in the hol_light shell",
+          "timestamp": "2025-10-30T16:30:41+08:00",
+          "tree_id": "dab74f88283b8f3b376d023d344402ff2bc592b1",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/f6d6c37e812898127dccb33ffc2cabd47bb1776e"
+        },
+        "date": 1761813210252,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 243172,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 693037,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 234768,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 399188,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1099202,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 377717,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 661215,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1443565,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 642172,
             "unit": "cycles"
           }
         ]
