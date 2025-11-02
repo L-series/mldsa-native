046cc39d1b8294769355081f15e1a013955dbc00
github-action-benchmark <github@users.noreply.github.com>
2025-11-02 05:39:52 +0000

    add AMD EPYC 3rd gen (c6a) (customSmallerIsBetter) benchmark result for d518e10d02ad7a5b8e930f8078bda0819b9aa030

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762061989552,
+  "lastUpdate": 1762061992783,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -90196,6 +90196,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 193111,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d518e10d02ad7a5b8e930f8078bda0819b9aa030",
+          "message": "Merge pull request #570 from pq-code-package/ntt-again\n\nReimplement AArch64 NTT/iNTT",
+          "timestamp": "2025-11-02T13:36:31+08:00",
+          "tree_id": "bf0b296dc760cf4e22a1dccef56e98c96e4b49e9",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/d518e10d02ad7a5b8e930f8078bda0819b9aa030"
+        },
+        "date": 1762061961408,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 69140,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 185738,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 69270,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 119245,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 296344,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 115244,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 201777,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 385304,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 193502,
             "unit": "cycles"
           }
         ]
