2130ee09fda3ce9de1fc99223c31a3bef6c0ac7f
github-action-benchmark <github@users.noreply.github.com>
2025-11-03 12:41:43 +0800

    add Mac Mini (M1, 2020) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for a65b1d96998b8c94eaf41f6b9c225fbd0008551f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762144883719,
+  "lastUpdate": 1762144903231,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -58174,6 +58174,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 328850,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a65b1d96998b8c94eaf41f6b9c225fbd0008551f",
+          "message": "Merge pull request #594 from pq-code-package/aarch64-intt-twiddle-alignment\n\nAArch64 iNTT: Eliminate unaligned twiddle loads",
+          "timestamp": "2025-11-03T12:40:55+08:00",
+          "tree_id": "cc24a3754deb493ccf246f36141fc8a25f387dd5",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/a65b1d96998b8c94eaf41f6b9c225fbd0008551f"
+        },
+        "date": 1762144901735,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 115060,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 431658,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 122202,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 197077,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 701075,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 197689,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 325240,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 884670,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 328866,
             "unit": "cycles"
           }
         ]
