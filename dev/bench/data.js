2932af38b5a6751e13a91f09134596ec4439a875
github-action-benchmark <github@users.noreply.github.com>
2025-11-04 13:15:54 +0800

    add Mac Mini (M1, 2020) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 19d1e750331b087b07fae21af2f349a509932bf6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762233333772,
+  "lastUpdate": 1762233353938,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -59899,6 +59899,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 328855,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "19d1e750331b087b07fae21af2f349a509932bf6",
+          "message": "Fix C90 issues in example code\n\nThe C90 tests were not functional before as make clean would not clean the\nexample code. The previous commit fixed that and revealed some C90\nissues in the example code.\nThis commit fixes the C90 issues.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2025-11-04T13:14:51+08:00",
+          "tree_id": "f8eb39b7f2fa732c7fdba626597b71dcb0ee9026",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/19d1e750331b087b07fae21af2f349a509932bf6"
+        },
+        "date": 1762233352465,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 115054,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 431561,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 122129,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 197095,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 701023,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 197677,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 325255,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 884684,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 328869,
             "unit": "cycles"
           }
         ]
