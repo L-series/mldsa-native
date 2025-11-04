d004361dcf3e7d453da74594d908ca05faa8b8ec
github-action-benchmark <github@users.noreply.github.com>
2025-11-04 12:06:35 +0800

    add SpacemiT K1 8 (Banana Pi F3) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for c82bb2a6799623b40165e7ee2b433c5a45d4f2e8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762229191995,
+  "lastUpdate": 1762229195391,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -47822,6 +47822,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 2398545,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jammy.chiou1@gmail.com",
+            "name": "jammychiou1",
+            "username": "jammychiou1"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "c82bb2a6799623b40165e7ee2b433c5a45d4f2e8",
+          "message": "Update documentation comments for public API functions\n\nUpdate \"Arguments\" section to reflect the switch to array notation for\nkeys/seeds and other fixed-length buffers.\n\nWhile we're at it, we also sync the docs between sign.h and\nmldsa_native.h and adjust comment indentation/alignment. Other than\nthat, the comments are mostly unchanged.\n\nSigned-off-by: jammychiou1 <jammy.chiou1@gmail.com>",
+          "timestamp": "2025-11-04T12:02:14+08:00",
+          "tree_id": "f8e015a28de43c8f2eaf14cd2bded295217b0a58",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/c82bb2a6799623b40165e7ee2b433c5a45d4f2e8"
+        },
+        "date": 1762229117323,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 821795,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 3329595,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 919831,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 1396718,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 5449327,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 1463867,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 2298916,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 6803515,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 2399805,
             "unit": "cycles"
           }
         ]
